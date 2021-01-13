a3389126759e9c63132c301724fb72ae60e4764a
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-14 03:33:02 +0900

    componentDidCatch와 Sentry 연동

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: 'https://dc580e277a7a4953ba71bff7a329eea7@o504578.ingest.sentry.io/5591655',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
